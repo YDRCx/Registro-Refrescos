b457a86bd21b28b0a0bdb90fa2f78912058f8641
Yefferson <yefferson073@gmail.com>
2023-07-12 04:33:53 -0400

    Version 1.3 Se modifico la fecha, estilos, cambio de display flex a display grid, se agrego el input para 'se transfiere x dia', se acomodaron los decimales de algunos resultados

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const inputEfectivo = document.querySelector("#efectivo")
 const inputTransferencia = document.querySelector("#transferencia")
 const textareaDetalles = document.querySelector('#detalles')
 const inputDivisa = document.querySelector("#divisa")
+const inputFechaEntregaPunto = document.querySelector('#fechaEntregaPunto')
 const pGanancia = document.querySelector("#ganancia")
 const pGananciaDividida = document.querySelector('#gananciaDividida')
 const pGananciaYefferson = document.querySelector('#yefferson')
-const inputFecha = document.querySelector('#myDate')
+const inputFecha = document.querySelector('#fecha')
 const btnCopiar = document.querySelector('#copiar')
 const btnBorrar = document.querySelector('#borrar')
 //const inputDetalles = document.querySelector('#detalles')
@@ -36,7 +37,7 @@ inputCosto.addEventListener("input", restaInputs)
 setTimeout(restaInputs, 2000)
 
 
-/*Copiar texto a Portapapeles*/
+/***** Copiar texto a Portapapeles ******/
 
 btnCopiar.addEventListener("click", copiarTexto);
 
@@ -46,77 +47,50 @@ function copiarTexto() {
     const gananciaRedondeada = ganancia.toFixed(2)
     const gananciaDividida = gananciaRedondeada / 2;
 
-    /**/
+    /*****/
     const fechaAFormatear = inputFecha.value
     const fechaObjeto = new Date(fechaAFormatear)
     const dia = fechaObjeto.getDate();
     const mes = fechaObjeto.getMonth() + 1; // Los meses en JavaScript son base 0, por lo que se suma 1
     const anio = fechaObjeto.getFullYear();
-
     const fechaNuevoFormato = `${mes.toString().padStart(2, "0")}-${dia.toString().padStart(2, "0")}-${anio}`;
-    /**/
+    /******/
 
-  const texto =
+const texto =
 `*Refrescos*
 *${fechaNuevoFormato}*
   
 Total: ${inputTotal.value}
 Costo: ${inputCosto.value}
 Ganancia: ${gananciaRedondeada}
-÷2 :${gananciaDividida} c/u)
+÷2: ${gananciaDividida.toFixed(2)} (c/u)
   
 *Yefferson: ${(parseFloat(inputCosto.value) + gananciaDividida).toFixed(2)} Total*
   
 *Forma de Pago/Dinero*
-• ${inputPunto.value} Punto
-• ${inputEfectivo.value} Bs Efectivo
+• ${inputPunto.value} Punto (Se transfiere el ${inputFechaEntregaPunto.value})
+• ${inputEfectivo.value} Bs Efectivo
 • ${inputTransferencia.value} Transferencia
 • ${inputDivisa.value} Divisas
-• Detalles: ${textareaDetalles.value}
-`; // Reemplaza con el texto que deseas copiar
+
+• *Detalles:*
+${textareaDetalles.value}`;
 
   copiarTextoAPortapapeles(texto)
   console.log(texto);
 }
 
 function copiarTextoAPortapapeles(texto) {
-    navigator.clipboard.writeText(texto)
-    .then(() => {
-        console.log("Texto copiado al portapapeles: " + text);
-      })
-      .catch((error) => {
-        console.error("Error al copiar el texto: " + error);
-      });
+  navigator.clipboard.writeText(texto)
+  .then(() => {
+      console.log("Texto copiado al portapapeles: " + text);
+    })
+    .catch((error) => {
+      console.error("Error al copiar el texto: " + error);
+    });
 }
 
-/*
-Refrescos
-Sábado 08/07/23
-
-✓Total: 13.90$
-Costo: 10.79$
- Ganancia: 3.11$
- ÷2 : 1.55$ c/u)
-
-✓gananciaDividida: 12.34$ total
-
-✓Forma de Pago/Dinero
-•245.28bs punto (se transfiere el 09)
-•20 bs Efectivo
-•3$
-
-*/
 
 
 
 
-
-
-/*
-inputDetalles.addEventListener("keydown", function(event) {
-    if(event.key === "Enter") {
-        event.preventDefault();
-        inputDetalles.value += "\n";
-    }
-})
-*/
